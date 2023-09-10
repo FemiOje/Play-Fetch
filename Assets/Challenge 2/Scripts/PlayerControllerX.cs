c39ff6e8af264e1045de6b8a00de6f4a3fb42985
Femi Ojetokun <ojetokunakinwale@gmail.com>
2023-09-10 11:53:57 +0100

    Challenge completed

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    bool canSpawn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && canSpawn)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            canSpawn = false;
+            StartCoroutine("SpawnCountdown");
+
+        }
+    }
+
+    IEnumerator SpawnCountdown()
+    {
+        yield return new WaitForSeconds(1);
+        canSpawn = true;
+    }
+}
